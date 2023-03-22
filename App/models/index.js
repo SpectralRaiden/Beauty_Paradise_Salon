67ace6ec335dac416788981d39985ea088e95256
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-03-21 18:50:08 -0700

    fileStructure

--- App/models/index.js
+++ App/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Gallery = require('./Gallery');
-const Painting = require('./Painting');
+const Gallery = require('./Appointment');
+const Painting = require('./Service');
 
 Gallery.hasMany(Painting, {
   foreignKey: 'gallery_id',
