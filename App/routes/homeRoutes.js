0f3ce95d3c5a4058b4c13d7600c81540f90c8ebc
Jerome <jerome.p.egbujor@gmail.com>
2023-03-21 21:03:54 -0500

    Deleted non essential starter code information

--- App/routes/homeRoutes.js
+++ App/routes/homeRoutes.js
@@ -4,8 +4,6 @@ const paintingController = require('../controllers/paintingController');
 const withAuth = require('../utils/auth');
 
 // route to create/add a home using async/await
-router.get('/', galleryController.getGalleries);
-router.get('/gallery/:id', withAuth, galleryController.getSingleGallery);
-router.get('/painting/:id', withAuth, paintingController.getSinglePainting);
+
 
 module.exports = router;
