4bfb61b50bffd400162d8a1515ea81ba10c7c925
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-03-28 23:04:55 -0700

    added salon_db and UserController createUser

--- App/controllers/api/userController.js
+++ App/controllers/api/userController.js
@@ -1,82 +1,23 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-
-// Login route
-exports.getLoginPage = ('/login', (req, res) => {
-  // If the user is already logged in, redirect to the homepage
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  // Otherwise, render the 'login' template
-  res.render('login');
-});
-
+// Exports the createUser function
 exports.createUser = async (req, res) => {
-  try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+	try {
+		// Create a new user using the User model and request data
+		const dbUserData = await User.create({
+			fname: req.body.fname, // First name
+			lname: req.body.lname, // Last name
+			username: req.body.username, // Username
+			email: req.body.email, // Email
+			password: req.body.password, // Password
+		});
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		// Save session data
+		req.session.save(() => {
+			req.session.loggedIn = true; // Set loggedIn to true
+			res.status(200).json(dbUserData); // Send a 200 response with user data
+		});
+	} catch (err) {
+		console.log(err); // Log error
+		res.status(500).json(err.errors[0].message); // Send a 500 response with error message
+	}
 };
 
-// Login
-exports.loginUser = async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
-
-    const validPassword = await dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res
-        .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-};
-
-// Logout
-exports.logOutUser = (req, res) => {
-  // When the user logs out, the session is destroyed
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-};
