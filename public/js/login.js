bcf4b1aef15f4e19e789536ad78a1519efe6cbd7
Christian Plasencia <113798773+Chris70A@users.noreply.github.com>
2023-03-29 17:54:27 -0700

    fixed issue with login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,37 @@
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+const loginFormHandler = async (event) => {                         // Login handler
+  event.preventDefault();                                           // Stop refresh
 
-  const username = document.querySelector("#username").value.trim();                            // Assigns trimmed username value.
-  const password = document.querySelector("#password").value.trim();                            // Assigns trimmed password value.
+  const username = document.querySelector("#username").value.trim();  // Get username
+  const password = document.querySelector("#password").value.trim();    // Get password
 
-  if (username && password) {                                                                   // Checks login credentials
-    const response = await fetch("/api/users/login", {                                          // Performs a network request.
+  if (username && password) {                                           // Check fields
+    const response = await fetch("/api/users/login", {                  // Fetch login
       method: "POST",
-      body: JSON.stringify({ username, password }),                                             // Converts object to string.
-      headers: { "Content-Type": "application/json" },                                          // Sets Content-Type header of HTTP request to application/json.
+      body: JSON.stringify({ username, password }),                     // JSON body
+      headers: { "Content-Type": "application/json" },                  // Set headers
     });
 
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert('Failed to log in.');                                                                 // Show the error message in a modal
-      
+    if (response.ok) {                                                    // On success
+      document.location.replace("/");                                     // Redirect
+    } else {                                                              // Show error
+      // alert('Failed to log in.');
+      // Show the error message in a modal
+      showErrorModal("Failed to log in.");
     }
+  } else {
+    // alert('Please fill out all fields.');
+    // Show the error message in a modal
+    showErrorModal("Please fill out all fields.");
+  }
 };
 
-
+// Define a function to show the error message in a modal
+function showErrorModal(message) {
+  // Update the content of the modal with the error message
+  document.getElementById("errorMessage").innerText = message;
+  // Show the modal
+  $("#errorModal").modal("show");
+}
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
