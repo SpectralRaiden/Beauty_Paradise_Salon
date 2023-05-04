bfbc52ab1b77969c550ec07dc96c47e5be6c71d9
Jacob Murray <jakeskata@gmail.com>
2023-05-04 17:00:15 -0400

    modified calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,10 +1,31 @@
 let now = Date.now();
-let oneDay = 60*60*24*1000;
+let oneDay = 60 * 60 * 24 * 1000;
 let thisMonth = new Date(now).getMonth();
 
 while (thisMonth == new Date().getMonth()) {
-    calendar.innerHTML += `<div class="date">${new Date(now).toDateString()}</div>`;
-    now+=oneDay;
+    let day = new Date(now);
+
+
+    calendar.innerHTML += `
+        <div class="date" onclick="handleDay(${now})">
+            <h5><b>${day.getDate()}</b></h5>
+        
+        </div>
+    `;
+    now += oneDay;
     thisMonth = new Date(now).getMonth();
 }
 
+const handleDay = d => {
+    let day = new Date(d).toDateString();
+    calendar.innerHTML = `
+        <div class="day">
+                <h1>${day}</h1>
+        </div>
+    `;
+
+    for (let i = 1; i < 25; i++) {
+        document.querySelector('.day').innerHTML += `<h3>${i} <input /></h3>`
+
+    };
+};
